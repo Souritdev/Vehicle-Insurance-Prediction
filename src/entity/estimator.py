0b71c2252cf6da8f4306e543e185d1bfc964fd2b
Sourit Nandy <souritnandy123@gmail.com>
2025-09-01 11:10:03 +0530

    CICD Test Done

--- src/entity/estimator.py
+++ src/entity/estimator.py
@@ -1,5 +1,4 @@
 import sys
-
 import pandas as pd
 from pandas import DataFrame
 from sklearn.pipeline import Pipeline
@@ -7,46 +6,46 @@ from sklearn.pipeline import Pipeline
 from src.exception import MyException
 from src.logger import logging
 
+
 class TargetValueMapping:
     def __init__(self):
-        self.yes:int = 0
-        self.no:int = 1
+        self.yes: int = 0
+        self.no: int = 1
+
     def _asdict(self):
         return self.__dict__
+
     def reverse_mapping(self):
         mapping_response = self._asdict()
-        return dict(zip(mapping_response.values(),mapping_response.keys()))
+        return dict(zip(mapping_response.values(), mapping_response.keys()))
+
 
 class MyModel:
+    """
+    Wrapper class for trained model and preprocessing pipeline
+    """
     def __init__(self, preprocessing_object: Pipeline, trained_model_object: object):
-        """
-        :param preprocessing_object: Input Object of preprocesser
-        :param trained_model_object: Input Object of trained model 
-        """
         self.preprocessing_object = preprocessing_object
         self.trained_model_object = trained_model_object
 
     def predict(self, dataframe: pd.DataFrame) -> DataFrame:
-        """
-        Function accepts preprocessed inputs (with all custom transformations already applied),
-        applies scaling using preprocessing_object, and performs prediction on transformed features.
-        """
         try:
             logging.info("Starting prediction process.")
 
-            # Step 1: Apply scaling transformations using the pre-trained preprocessing object
-            transformed_feature = self.preprocessing_object.transform(dataframe)
+            if not isinstance(self.preprocessing_object, Pipeline):
+                raise MyException("preprocessing_object is not a valid sklearn Pipeline", sys)
 
-            # Step 2: Perform prediction using the trained model
+            # Apply preprocessing transformations
+            transformed_features = self.preprocessing_object.transform(dataframe)
+
+            # Make predictions
             logging.info("Using the trained model to get predictions")
-            predictions = self.trained_model_object.predict(transformed_feature)
-
+            predictions = self.trained_model_object.predict(transformed_features)
             return predictions
 
         except Exception as e:
             logging.error("Error occurred in predict method", exc_info=True)
             raise MyException(e, sys) from e
-
 
     def __repr__(self):
         return f"{type(self.trained_model_object).__name__}()"
--- src/entity/s3_estimator.py
+++ src/entity/s3_estimator.py
@@ -1,62 +1,63 @@
+import sys
+from pandas import DataFrame
 from src.cloud_storage.aws_storage import SimpleStorageService
 from src.exception import MyException
 from src.entity.estimator import MyModel
-import sys
-from pandas import DataFrame
 
 
 class Proj1Estimator:
     """
-    This class is used to save and retrieve our model from s3 bucket and to do prediction
+    Class to handle S3 model retrieval and prediction
     """
-
-    def __init__(self,bucket_name,model_path,):
-        """
-        :param bucket_name: Name of your model bucket
-        :param model_path: Location of your model in bucket
-        """
+    def __init__(self, bucket_name: str, model_path: str):
         self.bucket_name = bucket_name
-        self.s3 = SimpleStorageService()
         self.model_path = model_path
-        self.loaded_model:MyModel=None
+        self.s3 = SimpleStorageService()
+        self.loaded_model: MyModel = None
 
-
-    def is_model_present(self,model_path):
+    def is_model_present(self, **kwargs) -> bool:
+        """
+        Check if model exists in S3
+        Accepts keyword argument 'model_path'
+        """
         try:
+            model_path = kwargs.get("model_path", self.model_path)
             return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=model_path)
         except MyException as e:
             print(e)
             return False
 
-    def load_model(self,)->MyModel:
+    def load_model(self) -> MyModel:
         """
-        Load the model from the model_path
-        :return:
-        """
-
-        return self.s3.load_model(self.model_path,bucket_name=self.bucket_name)
-
-    def save_model(self,from_file,remove:bool=False)->None:
-        """
-        Save the model to the model_path
-        :param from_file: Your local system model path
-        :param remove: By default it is false that mean you will have your model locally available in your system folder
-        :return:
+        Load model from S3
         """
         try:
-            self.s3.upload_file(from_file,
-                                to_filename=self.model_path,
-                                bucket_name=self.bucket_name,
-                                remove=remove
-                                )
+            loaded_obj = self.s3.load_model(self.model_path, bucket_name=self.bucket_name)
+            if not isinstance(loaded_obj, MyModel):
+                raise MyException(
+                    f"Loaded object is not MyModel instance. Got {type(loaded_obj)} instead", sys
+                )
+            return loaded_obj
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+    def save_model(self, from_file: str, remove: bool = False) -> None:
+        """
+        Upload model file to S3
+        """
+        try:
+            self.s3.upload_file(
+                from_file,
+                to_filename=self.model_path,
+                bucket_name=self.bucket_name,
+                remove=remove
+            )
         except Exception as e:
             raise MyException(e, sys)
 
-
-    def predict(self,dataframe:DataFrame):
+    def predict(self, dataframe: DataFrame):
         """
-        :param dataframe:
-        :return:
+        Make prediction using loaded model
         """
         try:
             if self.loaded_model is None:
