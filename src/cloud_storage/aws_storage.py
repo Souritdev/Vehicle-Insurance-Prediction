ba5a426d91b8dd038a8317f4d335c40654a54ecb
Sourit Nandy <souritnandy123@gmail.com>
2025-09-01 11:24:41 +0530

    CICD Test Done

--- src/cloud_storage/aws_storage.py
+++ src/cloud_storage/aws_storage.py
@@ -1,79 +1,59 @@
 import boto3
-from io import BytesIO, StringIO
-import os
-import sys
 import pickle
-from pandas import DataFrame, read_csv
+import sys
 from botocore.exceptions import ClientError
-from mypy_boto3_s3.service_resource import Bucket
-
 from src.exception import MyException
 from src.logger import logging
-from src.configuration.aws_connection import S3Client
 
 
 class SimpleStorageService:
-    """
-    Class to interact with AWS S3 storage for uploading, downloading, and deleting files.
-    """
-
     def __init__(self):
-        s3_client = S3Client()
-        self.s3_resource = s3_client.s3_resource
-        self.s3_client = s3_client.s3_client
-
-    def s3_key_path_available(self, bucket_name, s3_key) -> bool:
         try:
-            bucket = self.get_bucket(bucket_name)
-            file_objects = [file_object for file_object in bucket.objects.filter(Prefix=s3_key)]
-            return len(file_objects) > 0
+            self.s3_client = boto3.client("s3")
         except Exception as e:
-            raise MyException(e, sys)
+            raise MyException(e, sys) from e
 
-    def get_bucket(self, bucket_name: str) -> Bucket:
+    def s3_key_path_available(self, bucket_name: str, s3_key: str) -> bool:
+        """
+        Check if a specific object exists in S3
+        """
         try:
-            return self.s3_resource.Bucket(bucket_name)
-        except Exception as e:
-            raise MyException(e, sys)
+            response = self.s3_client.list_objects_v2(Bucket=bucket_name, Prefix=s3_key)
+            # response.get("Contents") can be None if key does not exist
+            contents = response.get("Contents", [])
+            if contents:
+                logging.info(f"S3 key {s3_key} found in bucket {bucket_name}")
+                return True
+            logging.info(f"S3 key {s3_key} NOT found in bucket {bucket_name}")
+            return False
+        except ClientError as e:
+            raise MyException(e, sys) from e
 
-    def get_file_object(self, filename: str, bucket_name: str):
+    def upload_file(self, from_file: str, to_filename: str, bucket_name: str, remove: bool = False) -> None:
+        """
+        Upload a local file to S3
+        """
         try:
-            bucket = self.get_bucket(bucket_name)
-            file_objects = [obj for obj in bucket.objects.filter(Prefix=filename)]
-            if len(file_objects) == 0:
-                raise FileNotFoundError(f"{filename} not found in bucket {bucket_name}")
-            return file_objects[0]
-        except Exception as e:
-            raise MyException(e, sys)
-
-    def read_object(self, s3_object, decode=True, make_readable=False):
-        try:
-            body = s3_object.get()["Body"].read()
-            if decode:
-                body = body.decode()
-            if make_readable:
-                return StringIO(body)
-            return body
-        except Exception as e:
-            raise MyException(e, sys)
-
-    def upload_file(self, from_filename: str, to_filename: str, bucket_name: str, remove: bool = True):
-        try:
-            self.s3_resource.meta.client.upload_file(from_filename, bucket_name, to_filename)
+            self.s3_client.upload_file(Filename=from_file, Bucket=bucket_name, Key=to_filename)
+            logging.info(f"Uploaded {from_file} to s3://{bucket_name}/{to_filename}")
             if remove:
-                os.remove(from_filename)
+                import os
+                os.remove(from_file)
+                logging.info(f"Removed local file: {from_file}")
         except Exception as e:
-            raise MyException(e, sys)
+            raise MyException(e, sys) from e
 
-    def load_model(self, model_name: str, bucket_name: str):
+    def load_model(self, model_path: str, bucket_name: str):
         """
-        Load a pickled MyModel object from S3
+        Load a pickled model from S3
         """
         try:
-            file_obj = self.get_file_object(model_name, bucket_name)
-            model_bytes = self.read_object(file_obj, decode=False)
+            import io
+            logging.info(f"Loading model from s3://{bucket_name}/{model_path}")
+            obj = self.s3_client.get_object(Bucket=bucket_name, Key=model_path)
+            model_bytes = obj["Body"].read()
             model = pickle.loads(model_bytes)
-            logging.info(f"Model {model_name} loaded from S3 bucket {bucket_name}")
+            logging.info(f"Model loaded successfully from S3")
             return model
         except Exception as e:
-            raise MyException(e, sys)
+            raise MyException(e, sys) from e
--- src/entity/s3_estimator.py
+++ src/entity/s3_estimator.py
@@ -7,61 +7,47 @@ from src.entity.estimator import MyModel
 
 class Proj1Estimator:
     """
-    Class to handle S3 model retrieval and prediction
+    This class is used to save and retrieve our model from S3 and do predictions
     """
-    def __init__(self, bucket_name: str, model_path: str):
-        self.bucket_name = bucket_name
-        self.model_path = model_path
-        self.s3 = SimpleStorageService()
-        self.loaded_model: MyModel = None
 
-    def is_model_present(self, **kwargs) -> bool:
-        """
-        Check if model exists in S3
-        Accepts keyword argument 'model_path'
-        """
+    def __init__(self, bucket_name: str, model_path: str):
         try:
-            model_path = kwargs.get("model_path", self.model_path)
-            return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=model_path)
-        except MyException as e:
-            print(e)
-            return False
+            self.bucket_name = bucket_name
+            self.model_path = model_path
+            self.s3 = SimpleStorageService()
+            self.loaded_model: MyModel = None
+        except Exception as e:
+            raise MyException(e, sys) from e
+
+    def is_model_present(self) -> bool:
+        """Check if model exists in S3"""
+        try:
+            return self.s3.s3_key_path_available(bucket_name=self.bucket_name, s3_key=self.model_path)
+        except Exception as e:
+            raise MyException(e, sys) from e
 
     def load_model(self) -> MyModel:
-        """
-        Load model from S3
-        """
+        """Load the model from S3"""
         try:
-            loaded_obj = self.s3.load_model(self.model_path, bucket_name=self.bucket_name)
-            if not isinstance(loaded_obj, MyModel):
-                raise MyException(
-                    f"Loaded object is not MyModel instance. Got {type(loaded_obj)} instead", sys
-                )
-            return loaded_obj
+            if self.loaded_model is None:
+                self.loaded_model = self.s3.load_model(model_path=self.model_path, bucket_name=self.bucket_name)
+            return self.loaded_model
         except Exception as e:
             raise MyException(e, sys) from e
 
     def save_model(self, from_file: str, remove: bool = False) -> None:
-        """
-        Upload model file to S3
-        """
+        """Upload a local model file to S3"""
         try:
-            self.s3.upload_file(
-                from_file,
-                to_filename=self.model_path,
-                bucket_name=self.bucket_name,
-                remove=remove
-            )
+            self.s3.upload_file(from_file=from_file, to_filename=self.model_path,
+                                bucket_name=self.bucket_name, remove=remove)
         except Exception as e:
-            raise MyException(e, sys)
+            raise MyException(e, sys) from e
 
     def predict(self, dataframe: DataFrame):
-        """
-        Make prediction using loaded model
-        """
+        """Predict using the loaded model"""
         try:
             if self.loaded_model is None:
                 self.loaded_model = self.load_model()
-            return self.loaded_model.predict(dataframe=dataframe)
+            return self.loaded_model.predict(dataframe)
         except Exception as e:
-            raise MyException(e, sys)
+            raise MyException(e, sys) from e
